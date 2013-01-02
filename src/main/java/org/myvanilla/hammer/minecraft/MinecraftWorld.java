b1b65e3369f9ab687dff9e54faa8ab07f99534db
William Lebel <lebelwilliam@gmail.com>
2013-01-02 00:57:12 -0500

    Added a read me + Fixed year.

--- src/main/java/org/myvanilla/hammer/minecraft/MinecraftWorld.java
+++ src/main/java/org/myvanilla/hammer/minecraft/MinecraftWorld.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of Hammer.
  *
- * Copyright (c) 2012-2012, Spout LLC <http://www.spout.org/>
+ * Copyright (c) 2012-2013, Spout LLC <http://www.spout.org/>
  * Hammer is licensed under the Spout License Version 1.
  *
  * Hammer is free software: you can redistribute it and/or modify it under
